9b32344d60da68bbb534a291b8408db0b4a438fc
Jack Kraus <jackkraus3856@gmail.com>
2025-05-04 14:32:18 -0400

    Feat: Mailing List Preview

---
+++ flaskr/__init__.py
@@ -0,0 +1,85 @@
+import os
+from flask import Flask, render_template, request
+from .db import get_db
+
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev'
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/', methods=["GET"])
+    def email():
+        content = request.args.get("content")
+        social_links = [
+            # facebook
+            {
+                "url": "https://www.facebook.com/people/Freshta-Taeb/pfbid0pbEhVeAoyC1zNs4txi2AXAyxEFzweoWXjc68M7W6dDjDsDycDqwCqJaZCSGrsS7hl/",
+                "image": "https://img.icons8.com/?size=30&id=118466&format=png&color=012852"
+            },
+            # instagram
+            {
+                "url": "https://www.instagram.com/taeb4assembly/",
+                "image": "https://img.icons8.com/?size=30&id=32309&format=png&color=012852"
+            },
+            # linkedin
+            {
+                "url": "https://www.linkedin.com/company/taeb-for-assembly/posts/?feedView=all",
+                "image": "https://img.icons8.com/?size=30&id=8808&format=png&color=012852"
+            },
+            # tiktok
+            {
+                "url": "https://www.tiktok.com/@taeb4assembly",
+                "image": "https://img.icons8.com/?size=30&id=118638&format=png&color=012852"
+            },
+            # X
+            {
+                "url": "https://www.tiktok.com/@taeb4assembly",
+                "image": "https://img.icons8.com/?size=30&id=phOKFKYpe00C&format=png&color=012852"
+            },
+        ]
+        response = (get_db().table("mailing_list").select("*").execute())
+        print(list(map(lambda row: row["email"], response.data)))
+        return render_template('email/mailing_list.html', name="John Smith", content=content, social_links=social_links)
+    
+    @app.route('/login', methods=["GET"])
+    def login():
+        return render_template('authentication/login.html')
+    
+    @app.route('/login', methods=["POST"])
+    def login_post():
+        data = request.form
+        email = data.get("email")
+        password = data.get("password")
+        return {
+            "email": email,
+            "password": password
+        }
+
+    @app.route('/mailing_list', methods=["GET"])
+    def send_email():
+        return render_template("forms/send_email.html")
+
+    @app.route('/mailing_list/preview', methods=["POST"])
+    def check_email():
+        pass
+
+    
+    return app
---
+++ flaskr/db.py
@@ -0,0 +1,13 @@
+from flask import g
+
+import os
+from supabase import create_client, Client
+
+url: str = "https://vdktuvrsapclxnmqdieg.supabase.co"
+key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZka3R1dnJzYXBjbHhubXFkaWVnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYzODA2MTgsImV4cCI6MjA2MTk1NjYxOH0.iQwdvRGT0eOcnZe9Fyv9oFLNLF2CyleEdCjxBqxdbag"
+
+def get_db():
+    if 'db' not in g:
+        g.db: Client = create_client(url, key)
+    
+    return g.db
