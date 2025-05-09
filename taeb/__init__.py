48742cdf2028426b39b2bbd4069ba403fbd9897b
Jack Kraus <jackkraus3856@gmail.com>
2025-05-09 17:02:49 -0400

    Update __init__.py

--- taeb/__init__.py
+++ taeb/__init__.py
@@ -7,7 +7,6 @@ import auth, mailing_list, embed
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev',
-    DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
 )
 
 if None is None:
