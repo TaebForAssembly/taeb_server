2e92fb65a5a5dc7b565ac8a1086f00bc1f888c3f
Jack Kraus <jackkraus3856@gmail.com>
2025-05-12 23:03:25 -0400

    Form fix

--- test.py
+++ test.py
@@ -2,6 +2,6 @@ from api.index import create_app
 from dotenv import load_dotenv
 
 if __name__ == '__main__':
-    app = create_app()
     load_dotenv()
+    app = create_app()
     app.run(debug=True)
