7e7ddf9770492595529f458415721ac0aa0e800a
Jack Kraus <jackkraus3856@gmail.com>
2025-05-17 12:37:58 -0400

    Feat: Form Formatting

--- api/embed.py
+++ api/embed.py
@@ -2,7 +2,71 @@ from flask import Blueprint, render_template, request, redirect, url_for, jsonif
 
 bp = Blueprint('embed', __name__, url_prefix='/embed')
 
-@bp.route('/mailing_list_form', methods=["GET", "POST"])
+abbreviation_to_name = {
+    "AK": "Alaska",
+    "AL": "Alabama",
+    "AR": "Arkansas",
+    "AZ": "Arizona",
+    "CA": "California",
+    "CO": "Colorado",
+    "CT": "Connecticut",
+    "DE": "Delaware",
+    "FL": "Florida",
+    "GA": "Georgia",
+    "HI": "Hawaii",
+    "IA": "Iowa",
+    "ID": "Idaho",
+    "IL": "Illinois",
+    "IN": "Indiana",
+    "KS": "Kansas",
+    "KY": "Kentucky",
+    "LA": "Louisiana",
+    "MA": "Massachusetts",
+    "MD": "Maryland",
+    "ME": "Maine",
+    "MI": "Michigan",
+    "MN": "Minnesota",
+    "MO": "Missouri",
+    "MS": "Mississippi",
+    "MT": "Montana",
+    "NC": "North Carolina",
+    "ND": "North Dakota",
+    "NE": "Nebraska",
+    "NH": "New Hampshire",
+    "NJ": "New Jersey",
+    "NM": "New Mexico",
+    "NV": "Nevada",
+    "NY": "New York",
+    "OH": "Ohio",
+    "OK": "Oklahoma",
+    "OR": "Oregon",
+    "PA": "Pennsylvania",
+    "RI": "Rhode Island",
+    "SC": "South Carolina",
+    "SD": "South Dakota",
+    "TN": "Tennessee",
+    "TX": "Texas",
+    "UT": "Utah",
+    "VA": "Virginia",
+    "VT": "Vermont",
+    "WA": "Washington",
+    "WI": "Wisconsin",
+    "WV": "West Virginia",
+    "WY": "Wyoming",
+    "DC": "District of Columbia",
+    "AS": "American Samoa",
+    "GU": "Guam GU",
+    "MP": "Northern Mariana Islands",
+    "PR": "Puerto Rico PR",
+    "VI": "U.S. Virgin Islands",
+}
+
+@bp.route('/mailing_list_form', methods=["GET"])
 def mailing_list_form_oembed():
     preview = request.args.get("preview") is not None
     return render_template('embeds/mailing_list_embed.html', preview=preview)
+
+@bp.route('/volunteer_form', methods=["GET"])
+def volunteer_form_oembed():
+    preview = request.args.get("preview") is not None
+    return render_template('embeds/volunteer_embed.html', preview=preview, states=abbreviation_to_name)
