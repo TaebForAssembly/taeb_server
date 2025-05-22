851bac0598df6adc4806ce19b762d0d5999a24cf
Jack Kraus <jackkraus3856@gmail.com>
2025-05-22 17:53:03 -0400

    Feat: update volunteer

--- api/volunteer.py
+++ api/volunteer.py
@@ -1,29 +1,37 @@
 from flask import Blueprint, request
-from webargs.flaskparser import use_args, parser
+from webargs.flaskparser import abort, parser, use_args, use_kwargs
 from webargs import fields, validate
 from .data import state_dict
 
 bp = Blueprint('volunteer', __name__, url_prefix='/volunteer')
 
-'''
-    "address1" : fields.Str(required=True),
-    "address2" : fields.Str(required=True),
-    "state" : fields.Str(required=True, validate=validate.OneOf(state_dict.keys())),
-    "email" : fields.Email(required=True),
-    "availability" : fields.Str(required=True),
-    "tasks" : fields.List(fields.Str(required=True, validate=validate.OneOf(state_dict.keys()))),
-    "phone" : fields.Str(validate=[validate.Length(equal=10), lambda s : s.isnumeric()])
+user_args = {
+    #  name
+    "first_name" : fields.Str(required=True),
+    "last_name" : fields.Str(required=True),
 
-    try:
-        args = parser.parse(user_args, request)
-        print(args )
-    except Exception as e:
-        print(e)
-'''
+    # address
+    "address1" : fields.Str(required=True),
+    "address2" : fields.Str(),
+    "city" : fields.Str(required=True),
+    "state" : fields.Str(required=True, validate=validate.OneOf(state_dict.keys())),
+    "zip" : fields.Str(required=True),
+
+    # contact information
+    "email" : fields.Email(required=True),
+    "phone" : fields.Str(required=True),
+
+    # about
+    "personal" : fields.Str(required=True),
+    "availability" : fields.Str(required=True),
+    "tasks": fields.List(fields.Str())
+}
 
 @bp.route('/', methods=["POST"])
-def add_volunteer():
-    return {
-        "success" : True,
-        "message" : request.form.get("address1")
-    }
+@use_args(user_args, location='form')
+def add_volunteer(args):
+    return args
+
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    abort(error_status_code, errors=err.messages)
